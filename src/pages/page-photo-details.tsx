08bcfba3f08ee3cb7a461d6590ed91284943dbf8
ArthurFloro <arthur.floro2006@gmail.com>
2026-08-21 14:25:32 -0300

    feat(router): configura roteamento e estrutura base de páginas
    - Instala e configura a biblioteca React Router para o gerenciamento de navegação da aplicação.
    - Cria a estrutura inicial de páginas, englobando a página inicial (listagem) e a página de detalhes da foto.
    - Implementa a captura de parâmetros dinâmicos na URL utilizando o hook useParams, permitindo renderização baseada em dados específicos.
    - Estabelece a fundação do layout do projeto (guiado pelo Figma) para a futura construção de novos componentes estruturais.

---
+++ src/pages/page-photo-details.tsx
@@ -0,0 +1,16 @@
+import { useParams } from "react-router";
+import Text from "../components/text";
+
+
+export default function PagePhotoDetails() {
+  const { id } = useParams()
+
+  return (
+    <>
+      <Text variant="heading-medium">Página detalhe da foto</Text>
+      <hr />
+      <Text variant="heading-medium">ID da foto: {id}</Text>
+
+    </>
+  )
+}
