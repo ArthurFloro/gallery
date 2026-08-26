706bd1edbe9372e906dff2b305dd4bfc4aff7541
ArthurFloro <arthur.floro2006@gmail.com>
2026-08-26 14:46:44 -0300

    feat(search): implementa filtro de fotos integrado com API e URL
    - Cria o parâmetro de busca ´q´ (query string) para gerenciar o estado da pesquisa na URL utilizando a biblioteca
    uqs.
    - Ajusta o componente PhotoSearch para capturar a digitação, enviar o parâmetro para a API e renderizar apenas as fotos correspondentes.
    - Remove filtros antigos e desnecessários do código para manter a base limpa.
    - Adiciona renderização condicional ao PhotoSearch, garantindo que o input de busca seja exibido apenas nas rotas onde ele faz sentido.

--- src/components/photos-search.tsx
+++ src/components/photos-search.tsx
@@ -2,15 +2,19 @@ import InputText from "./input-text";
 import SearchIcon from "../assets/icons/search.svg?react"
 import React, { useCallback, useState } from "react";
 import { debounce } from "../helpers/utils";
+import usePhotos from "../contexts/photos/hooks/use-photos";
 
 export default function PhotosSearch() {
   const [inputValue, setInputValue] = useState("")
+  const { filters } = usePhotos()
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedSetValue = useCallback(
-    debounce((value: string) => console.log("valor com debounce", value), 200),
-    []
-  );
+    debounce((value: string) => {
+      filters.setQ(value)
+    }, 200),
+    [filters.setQ]
+  )
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     const value = e.target.value
