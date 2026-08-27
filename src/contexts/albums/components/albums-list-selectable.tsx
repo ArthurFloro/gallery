bb8cc737febed1038291a70cd28bc501b0964a13
ArthurFloro <arthur.floro2006@gmail.com>
2026-08-27 15:17:29 -0300

    feat(albums): implementa hook useFotoAlbums e otimiza requisições
    - Cria o hook customizado useFotoAlbums para centralizar a manipulação e o vínculo de fotos dentro dos álbuns.
    - Implementa o bloqueio (disable) dos inputs durante o andamento das requisições, prevenindo cliques e envios múltiplos pelo usuário.
    - Refatora a organização do código para evitar repetições desnecessárias de chamadas à API, melhorando o desempenho da aplicação.
    - Valida o fluxo de atualizações na interface para assegurar uma experiência de uso consistente e fluida.

--- src/contexts/albums/components/albums-list-selectable.tsx
+++ src/contexts/albums/components/albums-list-selectable.tsx
@@ -1,7 +1,9 @@
+import { useTransition } from "react"
 import Divider from "../../../components/divider"
 import InputCheckbox from "../../../components/input-checkbox"
 import Skeleton from "../../../components/skeleton"
 import Text from "../../../components/text"
+import usePhotoAlbums from "../../photos/hooks/use-photo-albums"
 import type { Photo } from "../../photos/models/photo"
 import type { Album } from "../models/album"
 
@@ -13,11 +15,14 @@ interface AlbumsListSelectableProps {
 
 export default function AlbumsListSelectable({ albums, photo, loading }: AlbumsListSelectableProps) {
 
+  const { managePhotoOnAlbum } = usePhotoAlbums();
+  const [isUpdatingPhoto, setIsUpdatingPhoto] = useTransition();
+
   function isChecked(albumId: string) {
     return photo?.albums.some(album => album.id === albumId)
   }
 
-  function handlePhotoAlbums(albumId: string) {
+  async function handlePhotoOnAlbums(albumId: string) {
     let albumsIds = []
 
     if (isChecked(albumId)) {
@@ -26,16 +31,19 @@ export default function AlbumsListSelectable({ albums, photo, loading }: AlbumsL
       albumsIds = [...photo.albums.map(album => album.id), albumId]
     }
 
-    console.log("Esses sao os albuns que vamos enviar para o backend", albumsIds)
+    setIsUpdatingPhoto(async () => {
+      await managePhotoOnAlbum(photo.id, albumsIds);
+    });
   }
 
   return (
     <ul className="flex flex-col gap-4">
-      {!loading && albums?.length > 0 && albums.map((album, index) => (
+      {!loading && photo && albums?.length > 0 && albums.map((album, index) => (
         <li key={album.id} >
           <div className="flex items-center justify-between gap-1">
             <Text variant="paragraph-large" className="truncate">{album.title}</Text>
-            <InputCheckbox defaultChecked={isChecked(album.id)} onClick={() => handlePhotoAlbums(album.id)} />
+            <InputCheckbox defaultChecked={isChecked(album.id)} onChange={() => handlePhotoOnAlbums(album.id)}
+              disabled={isUpdatingPhoto} />
           </div>
           {index !== albums.length - 1 && <Divider className="mt-4" />}
         </li>
